c720749e4fe4fd0c7026ea381bc1fb9195402292
Andrea Corbellini <corbellini.andrea@gmail.com>
2018-01-08 11:44:44 +0100

    Disable SQL database

--- core/teacup/settings.py
+++ core/teacup/settings.py
@@ -96,12 +96,7 @@ WSGI_APPLICATION = 'teacup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 
 
 # Internationalization
