1c06840071647d41f13c8d43aa7e9e72badc49ac
Andrea Corbellini <corbellini.andrea@gmail.com>
2018-01-09 15:23:09 +0100

    Rewrite stormctl

--- cli/setup.py
+++ cli/setup.py
@@ -5,8 +5,7 @@ setup(
     version='0.1',
     scripts=['stormctl'],
     install_requires=[
-        'PyYAML >= 3, < 4',
-        'jsonmodels >= 2.2, < 3',
-        'simple-rest-client >= 0.3, <= 0.4',
+        'perfectstorm-lib',
+        'pyyaml >= 3, < 4',
     ],
 )
