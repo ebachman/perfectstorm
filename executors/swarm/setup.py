be19f416bf7d4ea96fbd89c91fb13af1675dc4fb
Andrea Corbellini <corbellini.andrea@gmail.com>
2018-03-24 17:04:06 +0100

    executors: implement an agent for Docker Swarm
    The unmanaged Docker agent has been replaced by a new one based on
    Docker Swarm.

--- executors/swarm/setup.py
+++ executors/swarm/setup.py
@@ -29,9 +29,9 @@
 from setuptools import setup
 
 setup(
-    name='perfectstorm-executors-docker',
+    name='perfectstorm-executors-swarm',
     version='0.1',
-    scripts=['storm-docker'],
+    scripts=['storm-swarm'],
     install_requires=[
         'perfectstorm-lib',
         'requests-unixsocket >= 0.1, < 0.2',
