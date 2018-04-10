8e7c5a064a68f0567c6dc9db23e575d91dba334b
Andrea Corbellini <corbellini.andrea@gmail.com>
2018-04-10 14:19:55 +0800

    Add tests for Group creation

--- tests/test_create.py
+++ tests/test_create.py
@@ -2,7 +2,7 @@ import re
 
 import pytest
 
-from perfectstorm import Agent, Resource
+from perfectstorm import Agent, Group, Resource
 from perfectstorm.api import Heartbeat
 from perfectstorm.exceptions import APIRequestError, ValidationError
 
@@ -261,3 +261,69 @@ class TestResourceCreation(BaseTestDocumentCreationWithAgent):
             {'owner': ['Document with id=fake does not exist.']},
         ),
     ]
+
+
+class TestGroupCreation(BaseTestDocumentCreation):
+
+    model = Group
+
+    default_group = {
+        'name': None,
+        'query': {},
+        'services': [],
+        'include': [],
+        'exclude': [],
+    }
+
+    valid_data = [
+        (
+            {},
+            default_group,
+        ),
+        (
+            {'name': 'test'},
+            {**default_group, 'name': 'test'},
+        ),
+
+        # Simple query
+
+        (
+            {'query': {'x': 'y'}},
+            {**default_group, 'query': {'x': 'y'}},
+        ),
+
+        # Nested query
+
+        (
+            {'query': {'x': [{'y': 'z'}, 1, 2, 3]}},
+            {**default_group, 'query': {'x': [{'y': 'z'}, 1, 2, 3]}},
+        ),
+
+        # Queries containing special characters
+
+        (
+            {'query': {'a.b': 'c'}},
+            {**default_group, 'query': {'a.b': 'c'}},
+        ),
+        (
+            {'query': {'a$b': 'c'}},
+            {**default_group, 'query': {'a$b': 'c'}},
+        ),
+        (
+            {'query': {'a\0b': 'c'}},
+            {**default_group, 'query': {'a\0b': 'c'}},
+        ),
+    ]
+
+    def test_duplicate_names(self):
+        group1 = Group(name='x')
+        group2 = Group(name='x')
+
+        group1.save()
+
+        with pytest.raises(APIRequestError) as excinfo:
+            group2.save()
+
+        assert excinfo.value.response.json == {
+            'name': ['Dupe'],
+        }
