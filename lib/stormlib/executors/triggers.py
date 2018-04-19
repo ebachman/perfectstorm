07d2e8488049aa3563fe7fa79dafebf463f7ac17
Andrea Corbellini <corbellini.andrea@gmail.com>
2018-04-19 16:00:19 +0800

    lib: base the trigger executor on the events API
    Instead of polling for pending triggers every second, wait for
    trigger-related events.

--- lib/stormlib/executors/triggers.py
+++ lib/stormlib/executors/triggers.py
@@ -1,7 +1,7 @@
 import abc
 
-from .. import Trigger, Procedure
 from . import AgentExecutorMixin, PollingExecutor
+from .. import Trigger, Procedure, events
 
 
 class ProcedureRunner:
@@ -45,13 +45,28 @@ class ProcedureRunner:
 
 class ProcedureExecutor(AgentExecutorMixin, PollingExecutor):
 
+    def get_trigger_event_filter(self):
+        return events.EventFilter([
+            events.EventMask(event_type='created', entity_type='trigger'),
+            events.EventMask(event_type='updated', entity_type='trigger'),
+        ])
+
     def get_pending_triggers(self):
         return Trigger.objects.filter(status='pending')
 
     def poll_jobs(self):
+        event_filter = self.get_trigger_event_filter()
+
+        with event_filter(events.stream()) as stream:
+            while True:
+                pending_triggers = self.get_pending_triggers()
+                if pending_triggers:
+                    break
+                next(stream)
+
         return [
-            self.procedure_runner(self.agent, trigger)
-            for trigger in self.get_pending_triggers()
+            self.get_procedure_runner(self.agent, trigger)
+            for trigger in pending_triggers
         ]
 
     @abc.abstractmethod
