4f70d78b5d7d9aea4fe6557958d40ba4719c18e6
Andrea Corbellini <corbellini.andrea@gmail.com>
2018-04-27 17:44:04 +0800

    lib: improve event filtering

--- lib/stormlib/events.py
+++ lib/stormlib/events.py
@@ -44,23 +44,66 @@ class EventMask(namedtuple(
         'BaseEventMask', 'event_type entity_type entity_id entity_names')):
 
     def __new__(
-            cls, event_type=None, entity=None,
-            entity_type=None, entity_id=None, entity_names=None):
-        if entity is not None:
-            if (entity_type is not None or
-                    entity_id is not None or
-                    entity_names is not None):
-                raise TypeError(
-                    'entity cannot be specified together with entity_type, '
-                    'entity_id or entity_names')
-            entity_type = type(entity).__name__.lower()
-            entity_id = entity.id
-
+            cls, event_type=None, entity_type=None, entity_id=None,
+            entity_names=None):
         if entity_names is not None:
             entity_names = frozenset(entity_names)
-
         return super().__new__(
             cls, event_type, entity_type, entity_id, entity_names)
+
+    @classmethod
+    def from_string(cls, s):
+        """
+        Construct an EventMask from a string in the following format:
+
+            '<event_type>:<entity_type>:<entity_id>:<name1>,<name2>,...'
+
+        Fields can be omitted and trailing colons are optional.
+
+        Examples:
+
+            EventMask.from_string(
+                'created:resource:res-4ANqadEgfdRKo8OKG956VA:nginx.1')
+            # is equivalent to:
+            EventMask(event_type='created',
+                `     entity_type='resource',
+                      entity_id='res-4ANqadEgfdRKo8OKG956VA',
+                      entity_names=['nginx.1'])
+
+            EventMask.from_string('created:resource')
+            # is equivalent to:
+            EventMask.from_string('created:resource::')
+            # is equivalent to:
+            EventMask(event_type='created',
+                `     entity_type='resource',
+                      entity_id=None,
+                      entity_names=None)
+
+            EventMask.from_string('::res-4ANqadEgfdRKo8OKG956VA')
+            # is equivalent to:
+            EventMask.from_string('::res-4ANqadEgfdRKo8OKG956VA:')
+            # is equivalent to:
+            EventMask(event_type=None,
+                `     entity_type=None,
+                      entity_id='res-4ANqadEgfdRKo8OKG956VA',
+                      entity_names=None)
+
+            EventMask.from_string(':::a,b,c')
+            # is equivalent to:
+            EventMask(event_type=None,
+                `     entity_type=None,
+                      entity_id=None,
+                      entity_names=['a', 'b', 'c'])
+        """
+        parts = s.split(':')
+
+        if len(parts) > 4:
+            raise ValueError(
+                'expected at most 4 colon-separated fields, got {!r}'
+                .format(s))
+
+        args = (item if item else None for item in parts)
+        return cls(*args)
 
     def matches(self, event):
         return (
@@ -71,7 +114,7 @@ class EventMask(namedtuple(
             (self.entity_id is None or
                 self.entity_id == event.entity.id) and
             (not self.entity_names or
-                not self.entity_names & set(event.entity.names))
+                self.entity_names & set(event.entity.names))
         )
 
 
@@ -139,16 +182,24 @@ class EventsStream:
 class EventFilter:
 
     def __init__(self, masks=()):
-        self.registered_events = set()
+        self.masks = set()
+        self.register_all(masks)
+
+    def register(self, event_mask):
+        if isinstance(event_mask, str):
+            event_mask = EventMask.from_string(event_mask)
+        self.masks.add(event_mask)
+
+    def register_all(self, masks):
         for event_mask in masks:
             self.register(event_mask)
 
-    def register(self, event_mask):
-        self.registered_events.add(event_mask)
+    def clear(self):
+        self.masks.clear()
 
     def match(self, event):
         return tuple(
-            event_mask for event_mask in self.registered_events
+            event_mask for event_mask in self.masks
             if event_mask.matches(event)
         )
 
@@ -156,7 +207,7 @@ class EventFilter:
         for event in stream:
             masks = self.match(event)
             if masks:
-                yield event, masks
+                yield event
 
     def __call__(self, stream):
         it = self._filter(stream)
@@ -166,9 +217,17 @@ class EventFilter:
             return it
 
 
-def latest(start=None, count=None, session=None):
-    return EventReader(session=session).latest(start, count)
+def latest(filters=None, start=None, count=None, session=None):
+    events = EventReader(session=session).latest(start, count)
+    if filters is not None:
+        event_filter = EventFilter(filters)
+        events = event_filter(events)
+    return events
 
 
-def stream(start=None, session=None):
-    return EventReader(session=session).stream(start)
+def stream(filters=None, start=None, session=None):
+    event_stream = EventReader(session=session).stream(start)
+    if filters is not None:
+        event_filter = EventFilter(filters)
+        event_stream = event_filter(event_stream)
+    return event_stream
